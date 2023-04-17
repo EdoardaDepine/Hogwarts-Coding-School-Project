5c6775db7d6b970c650779d842e0c2a5f5f09529
EdoardaDepine <edoarda.depine@gmail.com>
2023-04-17 18:04:55 -0300

    estilizando o projeto

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const buttonLogin = document.querySelector("#login");
 
+buttonLogin.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const inputEmail = document.querySelector("#input-name-login");
+  const inputSenha = document.querySelector("#input-password-login");
+
+  const inputEmailValue = inputEmail.value;
+  const inputSenhaValue = inputSenha.value;
+
+  inputsVerification(inputEmailValue, inputSenhaValue);
+
+  inputEmail.value = "";
+  inputSenha.value = "";
+});
+
+function inputsVerification(email, senha) {
+  if (email == "student@test.com" && senha == "123456") {
+    alert("Bem vindo!");
+  } else {
+    alert("Email ou senha inválidos!");
+  }
+}
+
+const textArea = document.querySelector("#textarea");
+const counter = document.querySelector("#counter");
+
+textArea.addEventListener("keyup", () => {
+  counter.innerText = 500 - textArea.value.length;
+});
