4792f57c17676e4f42504b9788c5fc2e210a0438
EdoardaDepine <edoarda.depine@gmail.com>
2023-04-18 08:35:04 -0300

    inserindo linhas para estilização

--- script.js
+++ script.js
@@ -39,11 +39,42 @@ buttonSubmit.addEventListener("click", (event) => {
   const lastName = document.querySelector("#input-lastname").value;
   const email = document.querySelector("#input-email").value;
   const selectHouse = document.querySelector("#house").value;
+  const selectFamily = verifyFamily();
+  const selectContents = verifyContents();
 
   form.innerHTML = `
    <div>Nome:${name}</div>
    <div>Sobrenome:${lastName}</div>
    <div>Email:${email}</div>
    <div>Casa:${selectHouse}</div>
+   <div>Família:${selectFamily}</div>
+   <div>Conteúdos>${selectContents}</div>
 `;
 });
+
+function verifyFamily() {
+  const inputsFamily = document.querySelectorAll(".radio-skil");
+
+  inputsFamily.forEach((input) => {
+    input.addEventListener("click", (event) => {
+      if (event.target.checked) {
+        return event.target.value;
+      }
+    });
+  });
+  //console.log(inputsFamily);
+}
+
+function verifyContents() {
+  const checkBoxes = document.querySelectorAll(".check-content");
+  const arr = [];
+
+  checkBoxes.forEach((check) => {
+    check.addEventListener("click", (event) => {
+      if (event.checked) {
+        return arr.push[event.target.value];
+      }
+    });
+  });
+  console.log(arr);
+}
